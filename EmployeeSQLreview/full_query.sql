267877ce27604bca25d11701696a4bb6d4c85619
Jennifer White <144621532+jnnfrwht14@users.noreply.github.com>
2023-12-03 11:47:04 -0600

    1203 deltas

---
+++ EmployeeSQLreview/full_query.sql
@@ -0,0 +1,13 @@
+--Run each select separately after importing each .csv file:
+
+SELECT * FROM titles;
+
+SELECT * FROM employees;
+
+SELECT * FROM departments;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM salaries;
+
+SELECT * FROM dept_manager;
